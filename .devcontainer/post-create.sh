82b38d6ac60a1a88368ffe043bc508a9668df55d
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-10-22 16:21:27 +0200

    Prettier PS1

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,6 @@
 wine .devcontainer/hello.exe
 cmake -B transpiler/build -S transpiler
 cmake --build transpiler/build
+
+# Shell prompt - only add if not already present
+grep -qF "PS1='\$(if [[ \"\$PWD\" == /workspaces/* ]];" ~/.bashrc || echo "PS1='\$(if [[ \"\$PWD\" == /workspaces/* ]]; then realpath --relative-to=/workspaces \"\$PWD\"; else echo \"\\w\"; fi)\\$ '" >> ~/.bashrc
